501af2e4358802ea24e3d442c69f8e852dc00fe3
deBabbbe <neotbh@gmx.de>
2023-03-01 08:33:28 +0000

    Move link menu to own component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,16 @@
 import type { NextPage } from "next";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Menu from "../components/Menu";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const router = useRouter();
   return (
     <>
-    <h1 id="header">Plannings</h1>
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <Link href="/customers">Customers</Link>
-        <Link href="/applications">Applications</Link>
-        <Link href="/employees">Employees</Link>
-        <Link href="/appointments">Appointments</Link>
-      </main>
-    </div>
+      <h1 id="header">Plannings</h1>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Menu></Menu>
+        </main>
+      </div>
     </>
   );
 };
